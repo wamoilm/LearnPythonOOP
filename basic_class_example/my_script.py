ba27247d0d91f89e880f8903ae6afecb9221180a
W Mohammed <wamoilm@gmail.com>
2025-03-07 00:27:28 +0000

    add a basic client usage script

---
+++ basic_class_example/my_script.py
@@ -0,0 +1,16 @@
+from basic_class_example.phone import Phone
+
+# create a basic phone called basic_phone_1
+# use the basic phone 1 for call
+
+# creating a basic phone
+basic_phone = Phone("B1", "TalkTalk  ", "TalkTalk_OS")
+
+# phone rings
+# phone takes the call
+# call finished phone hangs up
+
+basic_phone.ring()
+basic_phone.take_call()
+basic_phone.hangup_call()
+
